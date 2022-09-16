503aa1bf3c113e7bc9ba644c49f2becc93f1aa18
Joseph Tascona <joseph.tascona@gmail.com>
2022-09-16 16:44:59 -0400

    Added weather file and npm package

---
+++ weather.js
@@ -0,0 +1,11 @@
+const weather = require('weather-js');
+
+// Options:
+// search:     location name or zipcode
+// degreeType: F or C
+
+weather.find({ search: 'Barrie, ON', degreeType: 'C' }, function (err, result) {
+    if (err) console.log(err);
+
+    console.log(JSON.stringify(result, null, 2));
+});
